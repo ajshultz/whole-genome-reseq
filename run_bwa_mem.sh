b27cce278d73cc56a3788b7a17226070c8a7f287
ajshultz <allisonjshultz@gmail.com>
2014-10-03 21:41:02 -0400

    Updating mapping script
    Updating mapping script to match my database, etc.

--- run_bwa_mem.sh
+++ run_bwa_mem.sh
@@ -4,20 +4,24 @@
 #SBATCH -n 16
 #SBATCH -N 1
 #SBATCH --mem 8000
-#SBATCH -t 36:00:00
+#SBATCH -t 3:00:00
 #SBATCH -J bwa_mem
 #SBATCH -o bwa_mem_%j.out
 #SBATCH -e bwa_mem_%j.err
 
+module load centos6/bwa-0.7.7
+module load centos6/samtools-0.1.19-fasrc01_gcc-4.4.7
+
 GENOME=$1
 LIB=$2
 RUN=$3
+GENOMEDB=$4
 
-READ1="../data/trimmed/${GENOME}_${RUN}_${LIB}_1P.fastq.gz"
-READ2="../data/trimmed/${GENOME}_${RUN}_${LIB}_2P.fastq.gz"
-OUTPUT="$GENOME.$LIB.$RUN"
+READ1="../data/trimmed/${GENOME}_${RUN}_1P.fastq.gz"
+READ2="../data/trimmed/${GENOME}_${RUN}_2P.fastq.gz"
+OUTPUT="$GENOME.$RUN"
 
-bwa mem -t 16 -M -R '@RG\tID:'"$GENOME"'\tSM:'"$GENOME"'\tPL:Illumina_'"$RUN"'\tLB:'"$LIB"'\tPU:'"$LIB.$RUN" "${GENOME}1" $READ1 $READ2 | samtools view -b -T ${GENOME}1.fa.gz - > ${OUTPUT}.bam
+bwa mem -t 16 -M -R '@RG\tID:'"$GENOME"'\tSM:'"$GENOME"'\tPL:Illumina_'"$RUN"'\tLB:'"$LIB"'\tPU:'"$LIB.$RUN" "${GENOMEDB}" $READ1 $READ2 | samtools view -b -T ${GENOMEDB}.fa - > ${OUTPUT}.bam
 scontrol update JobId=$SLURM_JOBID NumNodes=1
 ./slurm_job_${SLURM_JOBID}_resize.sh 
 java -Xmx2g -jar ~/sw/progs/picard-tools-1.121/SortSam.jar TMP_DIR=/scratch INPUT=${OUTPUT}.bam OUTPUT=${OUTPUT}.sorted.bam SORT_ORDER=coordinate
