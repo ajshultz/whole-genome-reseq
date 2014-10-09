51d1f5144e9bace6b7b78fe11d57eabcb7c03488
Allison Shultz <ashultz@rclogin10.rc.fas.harvard.edu>
2014-10-09 16:28:10 -0400

    Changed mpileup module loading and time

--- run_mpileup.sh
+++ run_mpileup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH -p general
-#SBATCH -n 1
+#SBATCH -n 2
 #SBATCH -N 1
-#SBATCH --mem 8000
-#SBATCH -t 48:00:00
+#SBATCH --mem 16000
+#SBATCH -t 7-00:00:00
 #SBATCH -J mpileup
 #SBATCH -o mpileup_%j.out
 #SBATCH -e mpileup_%j.err
 
+source new-modules.sh
+module load samtools
 
-module load centos6/samtools-0.1.19-fasrc01_gcc-4.4.7
-
-samtools mpileup -ugDf /n/holyscratch/edwards_lab/HOFI_new/Genome/final.assembly.homo.fa ../mapping/*.clean.bam | bcftools view -cvg - > Au_all.raw.vcf 
+samtools mpileup -ugDf /n/regal/hofi/Genome/final.assembly.homo.fa ../mapping/*.clean.bam | bcftools view -cvg - > Au_all.raw.vcf 
