d044320606a41df554758beff9ddc28647182e8a
ajshultz <allisonjshultz@gmail.com>
2016-11-29 15:55:44 -0500

    Typo

--- PreProcessing/sumit_FastqToSam_jobs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sbatch /n/holylfs/LABS/edwards/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_FastqToSam.sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/rawdata/161109_D00742_0148_AC9ECWANXX/Lane8.indexlength_6/Fastq ME_01_01 ATGACT 1 C9ECWANXX 8 2016-11-09
