f5fe3b9f2377f756d16392eea127e40a1b1ed9f3
ajshultz <allisonjshultz@gmail.com>
2016-11-29 17:34:10 -0500

    Added brackets

--- PreProcessing/submit_FastqToSam_jobs.sh
+++ PreProcessing/submit_FastqToSam_jobs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_FastqToSam.sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/rawdata/161109_D00742_0148_AC9ECWANXX/Lane8.indexlength_6/Fastq/ ME_01_01 ATGACT 1 C9ECWANXX 8 2016-11-09
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_FastqToSam.sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/rawdata/161109_D00742_0148_AC9ECWANXX/Lane8.indexlength_6/Fastq ME_01_01 ATGACT 1 C9ECWANXX 8 2016-11-09
