77a6a7d82bbbeeca9b21daaa7c5f90e80892fd52
Allison Shultz <allisonjshultz@gmail.com>
2016-12-01 15:21:13 -0500

    New script to mark and remove duplicates, including testing

---
+++ PreProcessing/submit_Dedup_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_MarkDup_4reps.sbatch WA_14_16
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_MarkDup_4reps.sbatch NY_90_11
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_MarkDup_4reps.sbatch ME_01_01
