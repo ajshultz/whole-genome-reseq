861b9273f6101b0b8020e528b6a25310cbbc46ee
ajshultz <allisonjshultz@gmail.com>
2016-12-05 12:29:56 -0500

    Check if missing dedup, script for sorting, indexing, summarizing and validating

---
+++ PreProcessing/check_dedup.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import sys
+
+
+#Take a file with the list of all files in a directory, identify which libraries do not have deduped files, and prints those names to the screen. First input should be list of all files in directory, second should be original submission file.
+
+dirListFile = sys.argv[1]
+origSubmitFile = sys.argv[2]
+
+dirList = open(dirListFile,"r")
+origSubmit = open(origSubmitFile,"r")
+
+commands = {}
+
+for line in origSubmit:
+	if line[0] == "#":
+		pass
+	else:
+		splitline = line.strip().split(" ")
+		sample = splitline[2]
+		commands[sample] = line
+
+
+successful = []
+	
+for line in dirList:
+	splitline = line.strip().split(".")
+	if len(splitline) > 1:
+		if splitline[1] == "dedup":
+			successful.append(splitline[0])
+		else:
+			pass
+		
+for key in commands.keys():
+	if key not in successful:
+		print key+" has no dedup file."
+		
+
+dirList.close()
+origSubmit.close()
