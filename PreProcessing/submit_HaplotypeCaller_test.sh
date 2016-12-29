c85ea03dfac5a1d1bc7ea22deaa04031f1546bd6
ajshultz <allisonjshultz@gmail.com>
2016-12-29 11:55:27 -0500

    Scripts to run haplotype caller

---
+++ PreProcessing/submit_HaplotypeCaller_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_HaplotypeCaller.sbatch WA_14_16
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_HaplotypeCaller.sbatch NY_90_11
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PreProcessing/run_HaplotypeCaller.sbatch ME_01_01
