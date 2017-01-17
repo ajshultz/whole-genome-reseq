3b564dc26687ffb2d924e9c46896962d0e3b6473
ajshultz <allisonjshultz@gmail.com>
2017-01-16 21:51:01 -0500

    Outgroup BAM list, script to create anc fasta

--- ANGSD/submit_QualityDepthDistribution.sh
+++ ANGSD/submit_QualityDepthDistribution.sh
@@ -12,3 +12,5 @@ sleep 1
 sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_QualityDepthDistribution.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 800
 sleep 1
 sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_QualityDepthDistribution.sbatch CCCP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 800
+sleep `
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_QualityDepthDistribution.sbatch Outgroup /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 800
