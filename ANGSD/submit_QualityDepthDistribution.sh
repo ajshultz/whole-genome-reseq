1abab6dede9493e51b178e67a7ff2abf605e1f43
ajshultz <allisonjshultz@gmail.com>
2017-01-16 21:55:03 -0500

    Changed depth for outgroups.

--- ANGSD/submit_QualityDepthDistribution.sh
+++ ANGSD/submit_QualityDepthDistribution.sh
@@ -13,4 +13,4 @@ sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/ru
 sleep 1
 sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_QualityDepthDistribution.sbatch CCCP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 800
 sleep `
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_QualityDepthDistribution.sbatch Outgroup /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 800
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_QualityDepthDistribution.sbatch Outgroup /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 200
