a75efac7a7239b75bdc5e66fa7228636788a9892
ajshultz <allisonjshultz@gmail.com>
2017-01-22 15:13:43 -0500

    Fixed script name

--- ANGSD/submit_doCounts_SingleScaffold.sh
+++ ANGSD/submit_doCounts_SingleScaffold.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #sbatch sbatchscript samples bamlistdir mincov maxcov scaffold
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_LRT_Int20SplitAssoGLOpts.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 60 800 562
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_doCounts_SingleScaffold.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 60 800 562
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_LRT_Int20SplitAssoGLOpts.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 60 800 2
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_doCounts_SingleScaffold.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 60 800 2
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_LRT_Int20SplitAssoGLOpts.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 60 800 19
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_doCounts_SingleScaffold.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 60 800 19
 
