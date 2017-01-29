0c8d10c8dba8aa331d9ed973a7bd6cbbf2af0d57
ajshultz <allisonjshultz@gmail.com>
2017-01-29 08:02:21 -0500

    Removed min and max coverage, split up interval 20

--- ANGSD/submit_doCounts_Intervals_AllHF.sh
+++ ANGSD/submit_doCounts_Intervals_AllHF.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#sbatch sbatchscript samples bamlistdir mincov maxcov
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_doCounts_int.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists 1 15000
+#sbatch sbatchscript samples bamlistdir
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_doCounts_int.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_doCounts_int20.sbatch All_HF /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/BamLists
--- HaplotypeCalling/haplocaller_check.py
+++ HaplotypeCalling/haplocaller_check.py
@@ -67,56 +67,6 @@ def main(argv):
 	sampleList.close()
 	newVersionList.close()
 	output.close()
-'''
-	#List files in directory, and search for files with alignment_metrics or dedup.metrics	
-	for file in dirList:
-		fileSplit = file.strip().split(".")
-		if len(fileSplit)>2:
-			if fileSplit[-2] == "alignment_metrics":
-				aMet = open(file,"r")
-				for line in aMet:
-					splitline = line.split("\t")
-					try:
-					#Look for lines with the paired data at the sample level (no read group but sample in sample list, add to dict)
-						if splitline[-3] in samples and splitline [-2] == "" and splitline[0] == "PAIR":
-							alignMetDict[splitline[-3]] = line
-					except (IndexError):
-						pass
-			elif fileSplit[-2] == "metrics" and fileSplit[-3] == "dedup":
-				dMet = open(file,"r")
-				for line in dMet:
-					splitline = line.split("\t")
-					try:
-					#Look for lines with the first element in the sample list, add to dict
-						if splitline[0] in samples:
-							dedupMetDict[splitline[0]] = line
-					except (IndexError):
-						pass
-
-	
-	align = open(alignFile,"w")
-	dedup = open(dedupFile,"w")
-	
-	align.write("SAMPLE\tCATEGORY\tTOTAL_READS\tPF_READS\tPCT_PF_READS\tPF_NOISE_READS\tPF_READS_ALIGNED\tPCT_PF_READS_ALIGNED\tPF_ALIGNED_BASES\tPF_HQ_ALIGNED_READS\tPF_HQ_ALIGNED_BASES\tPF_HQ_ALIGNED_Q20_BASES\tPF_HQ_MEDIAN_MISMATCHES\tPF_MISMATCH_RATE\tPF_HQ_ERROR_RATE\tPF_INDEL_RATE\tMEAN_READ_LENGTH\tREADS_ALIGNED_IN_PAIRS\tPCT_READS_ALIGNED_IN_PAIRS\tBAD_CYCLES\tSTRAND_BALANCE\tPCT_CHIMERAS\tPCT_ADAPTER\tSAMPLE\tLIBRARY\tREAD_GROUP\n")
-	dedup.write("LIBRARY\tUNPAIRED_READS_EXAMINED\tREAD_PAIRS_EXAMINED\tSECONDARY_OR_SUPPLEMENTARY_RDS\tUNMAPPED_READS\tUNPAIRED_READ_DUPLICATES\tREAD_PAIR_DUPLICATES\tREAD_PAIR_OPTICAL_DUPLICATES\tPERCENT_DUPLICATION\tESTIMATED_LIBRARY_SIZE\n")
-	
-	for sample in samples:
-		if sample in alignMetDict.keys():
-			align.write(sample+"\t"+alignMetDict[sample])
-		elif sample not in alignMetDict.keys():
-			print sample+" does not have alignment metrics."
-	
-	for sample in samples:
-		if sample in dedupMetDict.keys():
-			dedup.write(dedupMetDict[sample])
-		elif sample not in dedupMetDict.keys():
-			print sample+" does not have dedup metrics."
-	
-
-	align.close()
-	dedup.close()
-'''
-
 
 
 if __name__ == "__main__":
