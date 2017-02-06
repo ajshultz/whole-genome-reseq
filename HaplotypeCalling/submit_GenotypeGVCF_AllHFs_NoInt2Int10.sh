eba4a0ffd19acceb5f1fc81c38a751ea9933076b
Allison Shultz <allisonjshultz@gmail.com>
2017-02-06 12:11:28 -0500

    Changed from array to specific submission

---
+++ HaplotypeCalling/submit_GenotypeGVCF_AllHFs_NoInt2Int10.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 1
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 3
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 4
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 5
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 6
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 7
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 8
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/HaplotypeCalling/run_GenotypeGVCF_AllHFsIntervalList.sbatch 9
