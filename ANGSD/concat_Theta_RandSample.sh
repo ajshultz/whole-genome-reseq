4a24fcdef3177c6d9d63978c0a49ad6afed17347
ajshultz <allisonjshultz@gmail.com>
2017-02-14 14:00:34 -0500

    trying out randomly sampling theta

---
+++ ANGSD/concat_Theta_RandSample.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SAMPLE=$1
+DIRECTORY=$2
+
+COUNT=0
+
+#Check if all files are present
+for i in {1..20}
+
+do
+
+if [ -f ${DIRECTORY}/${SAMPLE}_Int$i.thetas.gz ]
+then
+COUNT=$(($COUNT+1))
+
+else
+echo ${SAMPLE}_$i missing file
+fi
+
+done
+
+
+#If all files present copy first file to a new file to hold concatenated results and gunzip
+if [ "$COUNT" -eq "20" ]
+
+then	
+	
+	zcat ${DIRECTORY}/${SAMPLE}_Int$i.thetas.gz| head -n 1 > ${DIRECTORY}/${SAMPLE}_Rand.thetas
+	
+	for i in {1..2}
+		do	
+		zcat ${DIRECTORY}/${SAMPLE}_Int$i.thetas.gz | shuf -n 100 >> ${DIRECTORY}/${SAMPLE}_Rand.thetas	
+		done
+
+else
+	echo Something is wrong
+fi
