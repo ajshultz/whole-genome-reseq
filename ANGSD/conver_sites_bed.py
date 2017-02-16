e8ca7764913b3e4a8fe80186514a5bf093b5639a
ajshultz <allisonjshultz@gmail.com>
2017-02-15 23:06:56 -0500

    Convert sites format to bed format

---
+++ ANGSD/conver_sites_bed.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import re, sys, os, itertools, sets, getopt        # Load standard modules I often use
+import numpy, gzip
+
+'''
+Convert previous exclude sites file to bed file by subtracting 1 from first position
+'''
+
+def main(argv):
+	try:
+		opts,args = getopt.getopt(argv,'ho:i:')
+	except getopt.GetoptError:
+		print "convert_sites_bed.py -o <output file> -i <input file>"
+		sys.exit(2)
+			
+	outputFile=""
+	inputFile=""
+
+	for opt, arg in opts:
+		if opt == "-h":
+			print "convert_sites_bed.py -o <output file> -i <input file>"
+			sys.exit(2)
+		elif opt == "-o":
+			outputFile = arg
+		elif opt == "-i":
+			inputFile = arg
+
+	input = open(inputFile,"r")
+	output = open(outputFile,"w")
+	
+	for line in input:
+		line = line.strip().split("t")
+		line[1] = str(int(line[1])-1)
+		newline = "/t".join(line)
+		write.output(line+"\n")
+	
+	
+	input.close()
+	output.close()
+
+
+if __name__ == "__main__":
+		main(sys.argv[1:])
