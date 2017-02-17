e8a4e5687efeb2fab078da27c730a61df059eb4b
ajshultz <allisonjshultz@gmail.com>
2017-02-17 11:28:52 -0500

    add the concat back in

--- ANGSD/submit_catIntervals_ngsCovar.sh
+++ ANGSD/submit_catIntervals_ngsCovar.sh
@@ -2,10 +2,10 @@
 
 #samples nind nintervals
 
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch HFCCCP 226 16 results_pca
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch HFCCCP 225 16 results_pca
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch All_HF 210 19 results_pca
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch All_HF 209 16 results_pca
 sleep 1
 sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch Eastern_900114 146 19 results_pca
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch Western_0114 64 19 results_pca
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch Western_0114 63 19 results_pca
