775bbef74f197909a123daca5f62d2c2b08d9ba6
ajshultz <allisonjshultz@gmail.com>
2017-02-18 07:59:15 -0500

    typo

--- ANGSD/submit_SFS_Functional.sh
+++ ANGSD/submit_SFS_Functional.sh
@@ -2,80 +2,80 @@
 
 #samples bamlistdir sitesfile anc
 #Eastern_90: introns, cds, 3'UTR, 5'UTR, intergenic
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional_Intervals.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
-sleep 1
-
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional_Intervals.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional_Intervals.sbatch Eastern_90 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
 sleep 1
 
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional_Intervals.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
-sleep 1
-
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional_Intervals.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional_Intervals.sbatch Eastern_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
 sleep 1
 
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional_Intervals.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
-sleep 1
-
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
-sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional_Intervals.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional_Intervals.sbatch Eastern_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
 sleep 1
 
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SAF_Functional_Intervals.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional_Intervals.sbatch Western_01 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
+sleep 1
+
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional_Intervals.sbatch Western_14 /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
+sleep 1
+
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional_Intervals.sbatch CC /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
+sleep 1
+
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_introns.bed.angsdsites introns
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_cds.bed.angsdsites cds
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_three_prime_UTR.bed.angsdsites 3UTR
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/auto_five_prime_UTR.bed.angsdsites 5UTR
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Functional_Intervals.sbatch CP /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/genome/bedfiles/intergenic.bed.angsdsites intergenic
 sleep 1
 
