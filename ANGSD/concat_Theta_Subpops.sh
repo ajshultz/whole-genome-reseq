06232aa9c00a8155adcd6176db354973b0627c2c
Allison Shultz <allisonjshultz@gmail.com>
2017-02-23 11:15:54 -0500

    concatenate theta results for subpopulations (sliding window)

---
+++ ANGSD/concat_Theta_Subpops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAMES=( "AU_01" "AU_90" "AU_14" "CA_01" "CA_14" "IL_14" "MA_14" "ME_01" "ME_90" "NY_01" "NY_90" "OH_01" "OH_90" "WA_01" "WA_14" )
+NINT=20
+
+
+for i in ${NAMES[@]}
+do
+../whole-genome-reseq/ANGSD/concat_Theta_gz_and_sliding_int1.10.sh ${NAMES} results_theta_subpops
+../whole-genome-reseq/ANGSD/concat_Theta_gz_and_sliding.sh ${NAMES} results_theta_subpops
+done
