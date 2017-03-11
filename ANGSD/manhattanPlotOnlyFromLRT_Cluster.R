aa40d4b70fff411bd8f5011c3ee5f639571e97b1
ajshultz <allisonjshultz@gmail.com>
2017-03-10 20:54:21 -0500

    changed some of the plotting parameters

--- ANGSD/manhattanPlotOnlyFromLRT_Cluster.R
+++ ANGSD/manhattanPlotOnlyFromLRT_Cluster.R
@@ -7,6 +7,13 @@ dir <- args[2]
 
 load(paste(dir,"/",samples,".Asso1GL1.LRTPvals.Rdat",sep=""))
 
+#load("~/Dropbox/HFWGReseq/LRT/Eastern_90_Western_01.Asso1GL1.10K_Sample.Rdat")
+#load("~/Dropbox/HFWGReseq/LRT/Eastern_90_Western_01.Asso1GL1.Sig_FDR.Rdat")
+#e9001res <- sampled_res
+#e9001res <- rbind(e9001res,sig_fdr_e90w01)
+#samples <- "eastern 1990 vs. western 2001"
+#genomeinfo <- read.table("~/Dropbox/HFWGReseq/final.assembly.homo.fa.fai")
+
 #Read in genome info
 genomeinfo <- read.table("../genome/final.assembly.homo.fa.fai")
 genomeinfo$Scaff <- apply(genomeinfo,1,function(x)as.numeric(unlist(strsplit(as.character(x[1]),"_"))[2]))
@@ -16,9 +23,14 @@ names(scafflen) <-genomeinfo$Scaff
 maxfdrp <- min(e9001res$Pval.adj)
 maxp <- min(e9001res$Pval)
 bonferroni <- 0.05/nrow(e9001res)
+fdr <- max(e9001res[e9001res$Pval.adj<0.05,"Pval"])
 
 if (maxp == 0){
 	maxp <- 0.00000000000000001
+}
+
+if (maxfdrp == 0){
+	maxfdrp <- 0.00000000001
 }
 
 
@@ -36,15 +48,12 @@ cn=length(chrlengths)
 
 
 title=title
-fontsize=.5 #1.2
+fontsize=0.6 #0.5
 
-
-# File for plot
-#jpeg(filename="Manhattan_T.jpg",height=400,width=1000,quality=100) 			## might need to adjust NAME output file(e.g., HT_Manhattan.jpg)
 
 #layout(matrix(1:2,nrow=2))
 #par(mar=c(6,7,4,4))
-par(mar=c(3,3,2,1)+0.1)
+par(mar=c(1,3,2,1)+0.1)
 par(mgp=c(1.5,1,0))
 
 
@@ -69,9 +78,10 @@ cum.bp[chr==i] = bp[chr==i] + cum.ends[i-1]}
 
   ### Colors to use for chromosomes
  # use=c("red","blue")
- use=c("darkblue","black")
+ #use=c("#1f78b460","#1f78b4")
+ use=c("#a6cee3","#1f78b4")
  col=use[chr%%2+1]
- if (highlight==TRUE) col[special] = "red"
+ if (highlight==TRUE) col[special] = "#e31a1c"
   # chrcol=rep(c("darkblue","lightblue"),)
 
   # names(chrcol)=chrnumvec
@@ -93,12 +103,12 @@ plot(cum.bp, value,xlim=c(0,cum.ends[cn]),ylim=c(0,maxp),xaxs="i",xlab=xtxt,ylab
 
 
 if (highlight==TRUE){
-points(cum.bp[special],value[special],col=col[special],pch=19,cex=size)
+points(cum.bp[special],value[special],col=col[special],pch=19,cex=0.1)
 }
   ### Axes
   
 #axis(1, at = cum.centers, labels = plotnames,las=2,cex.axis=fontsize)
-axis(1, at = cum.centers, las=2,cex.axis=fontsize,labels=NULL)
+#axis(1, at = cum.centers, las=2,cex.axis=fontsize,labels=NULL)
 axis(2,las=1,cex.axis=fontsize)	
   
   # Horizontal line
@@ -108,7 +118,7 @@ if (line==TRUE) abline(h=t1,lty=1,col="black",lwd=1)
 
 
 png(file=paste(dir,"/",samples,".Asso1GL1_pvals_manhattan.png",sep=""),height=3,width=8,units="in",res=300)
-manhattan(chr=e9001res$Scaffold,bp=e9001res$Position,value=e9001res$Pval,chrlengths=scafflen,title=samples,plotnames=names(scafflen),ylab="-log p-value",t1=-log10(bonferroni),maxp=-log10(0.0000000000000001),logneg=TRUE,highlight=FALSE,line=TRUE)
+manhattan(chr=e9001res$Scaffold,bp=e9001res$Position,value=e9001res$Pval,chrlengths=scafflen,title=samples,plotnames=names(scafflen),ylab="-log p-value",t1=-log10(fdr),maxp=-log10(0.0000000000000001),logneg=TRUE,highlight=FALSE,line=TRUE,special=e9001res$Pval.adj<0.05)
 dev.off()
 
 png(file=paste(dir,"/",samples,".Asso1GL1_fdr_manhattan.png",sep=""),height=3,width=8,units="in",res=300)
