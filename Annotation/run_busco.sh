d07f38d15128cb01470dd2127f05b40a03982850
ajshultz <allisonjshultz@gmail.com>
2017-08-23 07:35:28 -0400

    more time

--- Annotation/run_busco.sh
+++ Annotation/run_busco.sh
@@ -2,8 +2,8 @@
 #SBATCH -p serial_requeue
 #SBATCH -n 16
 #SBATCH -N 1
-#SBATCH --mem 16000
-#SBATCH -t 5:00:00
+#SBATCH --mem 24000
+#SBATCH -t 1-00:00:00
 #SBATCH -J busco
 #SBATCH -o logs/busco_%j.out
 #SBATCH -e logs/busco_%j.err
@@ -12,4 +12,4 @@
 source activate busco
 
 #Run busco using avian lineages, and chicken augustus model
-run_BUSCO.py -i ../final.assembly.homo.fa -o HF_busco_res -l aves_odb9 -m genome -c 1 -sp chicken -f -c 16
+run_BUSCO.py -i ../final.assembly.homo.fa -o HF_busco_res -l aves_odb9 -m genome -c 1 -sp chicken -f -c 16 -r
