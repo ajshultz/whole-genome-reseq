dad423963c643f63714a1f1a6a3b8cef4ef7d765
Allison Shultz <allisonjshultz@gmail.com>
2017-12-04 14:33:29 -0500

    Removed relatives

--- Create_Ind_Lists.R
+++ Create_Ind_Lists.R
@@ -8,7 +8,7 @@ newdir <- "IndLists_NoRel"
 for (file in bamlists){
   print(file)
   if (sum(grep("BamList",file) > 0)) {
-  old <- read_table(paste("BamLists/",file,sep=""),col_names = c("path"))
+  old <- read_table(paste("BamLists_NoRel/",file,sep=""),col_names = c("path"))
   new <- old %>% separate(path,sep = "/",remove = FALSE,into = c("blank","n","holy","labs","lab","user","hfwgreseq","indel","merged","filename")) %>% select(filename,path) %>% separate(filename,sep="_realigned",into=c("ind","type")) %>% select(ind,path) %>% select(ind)
    newfile <- gsub(file,pattern = "_BamList.txt", replacement = "_IndList.txt")
   
