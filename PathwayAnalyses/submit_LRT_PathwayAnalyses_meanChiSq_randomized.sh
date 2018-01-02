fffe2fe7a7c176a0ff6d45f540549e122c351c55
Allison Shultz <allisonjshultz@gmail.com>
2018-01-02 11:54:17 -0500

    Now running mean ChiSq for random subsets

---
+++ PathwayAnalyses/submit_LRT_PathwayAnalyses_meanChiSq_randomized.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 1
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 2
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 3
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 4
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 5
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 6
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 7
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 8
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 9
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/PathwayAnalyses/run_LRT_PathwayAnalyses_meanChiSq_randomized.sbatch 10
