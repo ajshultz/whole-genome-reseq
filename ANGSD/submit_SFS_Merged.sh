90aa9afcf3f97ad1fc78a8a5d59faa01d7a89293
Allison Shultz <allisonjshultz@gmail.com>
2018-01-05 10:04:28 -0500

    Run SFS on merged subpops

---
+++ ANGSD/submit_SFS_Merged.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#samples
+
+#sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch AU_90 
+sleep 1
+#sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch AU_01 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch AU_14 
+sleep 1
+#sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch CA_01 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch CA_14 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch IL_14 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch MA_14 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch ME_90 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch ME_01 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch NY_90 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch NY_01 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch OH_90 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch OH_01 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch WA_01 
+sleep 1
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_SFS_Merged.sbatch WA_14 
+sleep 1
+
+
+
+
