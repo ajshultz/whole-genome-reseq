d46dc53f2e445e043f9cc3db8d3a343b6e8b3d11
Allison Shultz <allisonjshultz@gmail.com>
2018-01-11 11:48:45 -0500

    wrong filename

--- ANGSD/concat_Fst_gz_and_sliding.sh
+++ ANGSD/concat_Fst_gz_and_sliding.sh
@@ -26,18 +26,18 @@ if [ "$COUNT" -eq "20" ]
 
 then	
 	
-	zcat ${DIRECTORY}/${SAMPLE}_Int1.fst.gz > ${DIRECTORY}/${SAMPLE}_cat.fst
-	cp ${DIRECTORY}/${SAMPLE}_Int1.fst_1kbWin_1kbStep.pestPG ${DIRECTORY}/${SAMPLE}_cat_1.20.fst_1kbWin_1kbStep.pestPG
-		cp ${DIRECTORY}/${SAMPLE}_Int1.fst_2.5kbWin_.5kbStep.pestPG ${DIRECTORY}/${SAMPLE}_cat.fst_1.20_2.5kbWin_.5kbStep.pestPG
+	#zcat ${DIRECTORY}/${SAMPLE}_Int1.fst.gz > ${DIRECTORY}/${SAMPLE}_cat.fst
+	cp ${DIRECTORY}/${SAMPLE}_Int1.fst_1kbWin_1kbStep ${DIRECTORY}/${SAMPLE}_cat_1.20.fst_1kbWin_1kbStep
+		cp ${DIRECTORY}/${SAMPLE}_Int1.fst_2.5kbWin_.5kbStep ${DIRECTORY}/${SAMPLE}_cat.fst_1.20_2.5kbWin_.5kbStep
 	
 	for i in {1..20}
 		do	
-		cat ${DIRECTORY}/${SAMPLE}_Int${i}.fst_1kbWin_1kbStep.pestPG | tail -n +2 >> ${DIRECTORY}/${SAMPLE}_cat_1.20.fst_1kbWin_1kbStep.pestPG
-		cat ${DIRECTORY}/${SAMPLE}_Int${i}.fst_2.5kbWin_.5kbStep.pestPG | tail -n +2 >> ${DIRECTORY}/${SAMPLE}_cat_1.20.fst_2.5kbWin_.5kbStep.pestPG		
-		zcat ${DIRECTORY}/${SAMPLE}_Int${i}.fst.gz | tail -n +2 >> ${DIRECTORY}/${SAMPLE}_cat.fst
+		cat ${DIRECTORY}/${SAMPLE}_Int${i}.fst_1kbWin_1kbStep | tail -n +2 >> ${DIRECTORY}/${SAMPLE}_cat_1.20.fst_1kbWin_1kbStep
+		cat ${DIRECTORY}/${SAMPLE}_Int${i}.fst_2.5kbWin_.5kbStep | tail -n +2 >> ${DIRECTORY}/${SAMPLE}_cat_1.20.fst_2.5kbWin_.5kbStep		
+		#zcat ${DIRECTORY}/${SAMPLE}_Int${i}.fst.gz | tail -n +2 >> ${DIRECTORY}/${SAMPLE}_cat.fst
 		done
 
-	gzip ${DIRECTORY}/${SAMPLE}_cat.thetas
+	gzip ${DIRECTORY}/${SAMPLE}_cat.fst
 	
 else
 	echo Something is wrong
