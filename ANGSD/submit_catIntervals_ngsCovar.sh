cd07cb2fb69832c93e771bd117a9de13282e246c
Allison Shultz <allisonjshultz@gmail.com>
2018-01-11 12:24:08 -0500

    increase intervals

--- ANGSD/submit_catIntervals_ngsCovar.sh
+++ ANGSD/submit_catIntervals_ngsCovar.sh
@@ -2,9 +2,9 @@
 
 #samples nind nintervals
 
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch HFCCCP 225 16 results_pca
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch HFCCCP 225 19 results_pca
 sleep 1
-sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch All_HF 209 16 results_pca
+sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch All_HF 209 19 results_pca
 sleep 1
 sbatch /n/holylfs/LABS/edwards_lab/ashultz/HFWGReseq/whole-genome-reseq/ANGSD/run_catIntervals_ngsCovar.sbatch Eastern_900114 146 19 results_pca
 sleep 1
