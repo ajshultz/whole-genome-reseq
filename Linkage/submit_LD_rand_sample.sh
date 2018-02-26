8564b5bf2db9e8655ca1bb9e8cd0c8a544f2a794
Allison Shultz <allisonjshultz@gmail.com>
2018-02-26 15:29:25 -0500

    more memory

--- Linkage/submit_LD_rand_sample.sh
+++ Linkage/submit_LD_rand_sample.sh
@@ -4,11 +4,12 @@
 #Arguments: 1:population 2:proportion lines to pull 3:output directory 4:input file directory
 #For now going to try pulling 1% of lines
 sbatch ../whole-genome-reseq/Linkage/run_LD_rand_sample.sh Eastern_90_28inds 0.01 linkage_rand_samples linkage_28inds
-
+sleep 1
 sbatch ../whole-genome-reseq/Linkage/run_LD_rand_sample.sh Eastern_01_28inds 0.01 linkage_rand_samples linkage_28inds
-
+sleep 1
 sbatch ../whole-genome-reseq/Linkage/run_LD_rand_sample.sh Eastern_14_28inds 0.01 linkage_rand_samples linkage_28inds
-
+sleep 1
 sbatch ../whole-genome-reseq/Linkage/run_LD_rand_sample.sh Western_01_28inds 0.01 linkage_rand_samples linkage_28inds
-
+sleep 1
 sbatch ../whole-genome-reseq/Linkage/run_LD_rand_sample.sh Western_14_28inds 0.01 linkage_rand_samples linkage_28inds
+sleep 1
