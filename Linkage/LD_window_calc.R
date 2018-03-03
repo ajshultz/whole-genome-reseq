c0e7ef90dfdb8afde5fda5b206ceaac58276cf39
Allison Shultz <allisonjshultz@gmail.com>
2018-03-02 19:08:57 -0500

    added if statement for empty files

--- Linkage/LD_window_calc.R
+++ Linkage/LD_window_calc.R
@@ -22,6 +22,8 @@ ld %>%
 #read through remaining files, append to scaffold 0 file
 for (i in 1:7026){
   ld <- read_delim(paste0(pop,"_scaffold_",i,"_10kbMax.linkage"),delim="\t",skip=6,col_names=c("CHR_x","POS_x","CHR_y","POS_y","DIST","BEST_D","BEST_Dprime","BEST_D2","BEST_R2","ADJ_BEST_D","ADJ_BEST_Dprime","ADJ_BEST_D2","ADJ_BEST_R2","Ni","LOGLIKE"))
+  
+  if (nrow(ld) > 0){
   ld %>%
     head(n=-1) %>%
     mutate(POS_x = as.integer(POS_x/1000)) %>%
@@ -29,5 +31,8 @@ for (i in 1:7026){
     group_by(CHR_x,POS_x) %>%
     summarize(BEST_R2 =round(mean(BEST_R2),digits=3), ADJ_BEST_R2 = round(mean(ADJ_BEST_R2),digits=3), Ni = round(mean(Ni),digits=2)) %>%
     mutate(WinStart = as.integer(POS_x*1000), WinStop = as.integer(POS_x*1000+999)) %>%
-    select(CHR = CHR_x, WinStart, WinStop, BEST_R2, ADJ_BEST_R2, Ni) %>%    write_delim(paste0("_",pop,"_LD_calc_1kbwins.linkage"),delim = "\t",col_names = F,append=T)
+    select(CHR = CHR_x, WinStart, WinStop, BEST_R2, ADJ_BEST_R2, Ni) %>%    
+    write_delim(paste0("_",pop,"_LD_calc_1kbwins.linkage"),delim = "\t",col_names = F,append=T)
+  }
+  
 }
